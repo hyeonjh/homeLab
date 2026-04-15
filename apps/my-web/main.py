dd02928e470f4f355ec5ddc48a17f002f9b7fe52
hyeonjihoon <97426920+hyeonjh@users.noreply.github.com>
2026-04-16 00:28:54 +0900

    main 파일

---
+++ apps/my-web/main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class Item(BaseModel):
+    name: str
+    price: float
+    description: str | None = None
+
+@app.get("/")
+async def root():
+    return {"message": "FastAPI Web Server is running in k3s!"}
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int, q: str | None = None):
+    return {"item_id": item_id, "q": q}
